43fc7e8684570e7ebda0840730b847002a4cad29
David Reese <dpr989@mail.missouri.edu>
2016-03-10 10:02:50 -0800

    butt

--- src/HW2b.cpp
+++ src/HW2b.cpp
@@ -15,17 +15,18 @@ void parseObjFile(FILE* input);
 std::vector<GLfloat*> vertices;
 std::vector<GLint*> faces;
 
+int count = 0;
 float g_rotation = 0;
 float g_rotation_speed = 0.2f;
-string display_type = "t";
+string display_type = "l";
 
 //Vars for tracking the xyz min/max
-size_t maxx = 0;
-size_t maxy = 0;
-size_t maxz = 0;
-size_t minx = 0;
-size_t miny = 0;
-size_t minz = 0;
+int maxx = 0;
+int maxy = 0;
+int maxz = 0;
+int minx = 0;
+int miny = 0;
+int minz = 0;
 
 struct glutWindow{
 	int width;
@@ -70,96 +71,6 @@ void drawPolygons(){
 		}
 	glEnd();
 }
-
-void display(void){
-	glClearColor(0.0,0.0,1.0,0.0); //Set clear color to blue
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear color buffer and depth buffer
-	glLoadIdentity();
-	gluLookAt( 60,0,0, 0,0,0, 0,0,1);
-
-	glPushMatrix();										
-		glColor3f(1,0,0);
-		glTranslatef(2,0,0);							
-		//glRotatef(g_rotation,0,1,0);
-		glRotatef(95,0,1,0);
-		
-		if(display_type.compare("p")==0){
-			drawPoints();
-		}
-		else if(display_type.compare("l")==0){
-			drawLines();
-		}
-		else if(display_type.compare("t")==0){
-			drawPolygons();
-		}
-		else{
-			drawPolygons();
-		}
-		
-	glPopMatrix();
-	g_rotation += g_rotation_speed;
-	glutSwapBuffers();
-}
-
-
-void init(string filename) {
-	//Select projection matrix
-	glMatrixMode(GL_PROJECTION);
-	//Set viewport
-	glViewport(0,0,win.width, win.height);
-	//Reset projection matrix
-	glLoadIdentity();
-	GLfloat aspect = (GLfloat) win.width/win.height;
-	//Setup a perspective projection matrix
-	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);
-	//Specify with matrix is the current matrix
-	glMatrixMode(GL_MODELVIEW);
-	glShadeModel(GL_SMOOTH);
-	glClearDepth(1.0f);
-	glEnable(GL_DEPTH_TEST);
-   	glDepthFunc( GL_LEQUAL );
- 	glPointSize(10);
-
-	//Open each file
-	FILE* cube = fopen(filename.c_str(),"r");
-	parseObjFile(cube);	
-}
-
-void processKeys(unsigned char key, int x, int y) {
-	if (key == 'a')
-	// your code here
-	glutPostRedisplay();
-}
-
-int main(int argc, char** argv){
-	win.width = 700;
-	win.height = 700;
-	win.field_of_view_angle = 10;
-	win.z_near = 1.0f;
-	win.z_far = 500.0f;
-	string filename;
-	//cout << endl <<argv[1]<<endl;
-	if(argc > 2){
-		display_type = argv[1];
-		filename = argv[2];
-	}else if(argc == 2){
-		filename = argv[1];
-	}
-	else{
-		cout << "Error. Invalid input"<<endl;
-	}
-
-	glutInit(&argc, argv);
-  	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-  	glutInitWindowSize(win.width, win.width);
-  	glutCreateWindow("HW2");
-  	glutDisplayFunc(display);
-  	init(filename);
-  	//glutReshapeFunc(reshape);
-  	glutMainLoop();
-  return 0;
-}
-
 
 void parseObjFile(FILE* input){
 	faces.clear();
@@ -208,5 +119,138 @@ void parseObjFile(FILE* input){
 			}
 	}
 	cout << "\nThe max x:" << maxx << endl <<"The max y:"<< maxy << endl << "The max z:" << maxz << endl;
+	cout << "\nThe min x:" << minx << endl <<"The min y:"<< miny << endl << "The min z:" << minz << endl;
 	fclose(input);
+}
+
+void display(void){
+	glClearColor(0.0,0.0,1.0,0.0); //Set clear color to blue
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear color buffer and depth buffer
+	glMatrixMode(GL_MODELVIEW);
+	//Draw Coordinate System
+	if(1){
+		//Draw enpoints and origin
+		glBegin(GL_POINTS); // render with points
+			glColor3f(1,0,1);
+			glVertex3i(0,0,0); //display a point
+			glColor3f(1,0,0);
+			glVertex3f(0,3,0);
+			glColor3f(0,1,0);
+			glVertex3f(3,0,0);
+			glColor3f(1,1,0);
+			glVertex3f(0,0,3);
+		glEnd();
+	
+		//Draw coordinate lines
+		glBegin(GL_LINES);
+			//y coordinate
+			glColor3f(1,0,0);
+			glVertex3f(0,0,0);
+			glVertex3f(0,3,0);
+			//x coordinate
+			glColor3f(0,1,0);
+			glVertex3f(0,0,0);
+			glVertex3f(3,0,0);
+			//z coordinate
+			glColor3f(1,1,0);
+			glVertex3f(0,0,0);
+			glVertex3f(0,0,3);
+		glEnd();
+	}
+	
+	glLoadIdentity();
+	gluLookAt(45,0,0, 0,0,0, 0,0,1);
+
+	//glMatrixMode(GL_MODELVIEW);
+	cout << "Center of object:"<<(maxx+minx)/2<<", "<<(maxy+miny)/2<<", "<<(maxz+minz)/2<<endl;
+	glBegin(GL_POINTS);
+		glVertex3f((maxx+minx)/2,(maxy+miny)/2,(maxz+minz)/2);
+	glEnd();
+
+	glPushMatrix();										
+		glColor3f(1,0,0);
+		
+		//glScalef(0.5,0.5,0.5);
+		glTranslatef(-((maxx+minx)/2),-((maxy+miny)/2),-((maxz+minz)/2));
+		//cout <<endl<<"Translated x:"<<-((maxx+minx)/2)<<endl<<"Translated y:"<<-(maxy-miny)/2<<endl<<"Translated z:"<<-(maxz-minz)/2<<endl<<count;
+		
+		if(display_type.compare("p")==0){
+			drawPoints();
+		}
+		else if(display_type.compare("l")==0){
+			drawLines();
+		}
+		else if(display_type.compare("t")==0){
+			drawPolygons();
+		}
+		else{
+			drawPolygons();
+		}
+		
+	glPopMatrix();
+	g_rotation += g_rotation_speed;
+	glutSwapBuffers();
+}
+
+
+void init(string filename) {
+	//Select projection matrix
+	glMatrixMode(GL_PROJECTION);
+	//Set viewport
+	glViewport(0,0,win.width, win.height);
+	
+	//Reset projection matrix
+	glLoadIdentity();
+	GLfloat aspect = (GLfloat) win.width/win.height;
+	
+	//Setup a perspective projection matrix
+	gluPerspective(win.field_of_view_angle, aspect, win.z_near, win.z_far);
+	
+	//Specify with matrix is the current matrix
+	glMatrixMode(GL_MODELVIEW);
+	glShadeModel(GL_SMOOTH);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+   	glDepthFunc( GL_LEQUAL );
+ 	glPointSize(10);
+
+	//Open file and fill data structures
+	FILE* objectFile = fopen(filename.c_str(),"r");
+	parseObjFile(objectFile);	
+}
+
+void processKeys(unsigned char key, int x, int y) {
+	if (key == 'a')
+	// your code here
+	glutPostRedisplay();
+}
+
+int main(int argc, char** argv){
+	win.width = 700;
+	win.height = 700;
+	win.field_of_view_angle = 10;
+	win.z_near = 1.0f;
+	win.z_far = 500.0f;
+	string filename;
+	
+	if(argc > 2){
+		display_type = argv[1];
+		filename = argv[2];
+	}else if(argc == 2){
+		filename = argv[1];
+	}
+	else{
+		cout << "Error. Invalid input"<<endl;
+	}
+
+	glutInit(&argc, argv);
+  	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  	glutInitWindowSize(win.width, win.width);
+  	glutInitWindowPosition(200,1500);
+  	glutCreateWindow("HW2");
+  	glutDisplayFunc(display);
+  	init(filename);
+  	//glutReshapeFunc(reshape);
+  	glutMainLoop();
+  return 0;
 }
